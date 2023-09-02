7ae5f08044f9d62da6be6ad783c5d630cbbc68b7
shanu <shanu.shrivastava@students.iiit.ac.in>
2023-09-02 18:04:26 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {}, "I am h1 tag,    "),
+    React.createElement("h2", {}, "I am h2 tag,,"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
 ]);
-
-console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- readme.md
+++ readme.md
@@ -1,1 +1,21 @@
-Namste React 🚀
+# Namste React 🚀
+
+# Parcel
+
+- Dev build
+- Local server
+- HMR = Hot module replacement
+- File watching algorithm - written in c++
+- Caching - gives faster build
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent hashing
+- Code splitting
+- Diffrential bundling - to support older browser
+- Diagnostics
+- Error handling
+- Https
+- Tree shaking - remove unused code
+- Different dev and prod bundles
